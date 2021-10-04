d6d5a98732d23fe5de3336619e9813c11964fecd
Laeh03 <64225504+Laeh03@users.noreply.github.com>
2021-10-04 00:02:21 -0400

    13.5.6

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,51 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer(
+    'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+    });
+  
+  // We create the dark view tile layer that will be an option for our map.
+  let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+  });
+  
+  // Create a base layer that holds both maps.
+  let baseMaps = {
+    Street: streets,
+    "Satellite Streets": satelliteStreets
+  };
+  
+  // Create the map object with center, zoom level and default layer.
+  let map = L.map('mapid', {
+      center: [43.7, -79.3],
+      zoom: 11,
+      layers: [streets]
+  })
+  
+  let myStyle = {
+      color: "#0221cf",
+      weight: 1,
+    fillColor: "#adaa63",
+    fillOpacity: 0.8
+  }
+  // Pass our map layers into our layers control and add the layers control to the map.
+  L.control.layers(baseMaps).addTo(map);
+  
+  // Accessing the Toronto neighborhoods GeoJSON URL.
+  let torontoHoods = "https://raw.githubusercontent.com/tienl/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+  
+  
+  // Grabbing our GeoJSON data.
+  d3.json(torontoHoods).then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+      style: myStyle,
+      onEachFeature: function (features, layer){
+        layer.bindPopup("<h3> Neighborhood: " + features.properties.AREA_NAME + "</h3>");
+      }
+      }).addTo(map);
+    });
